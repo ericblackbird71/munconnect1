c7e28dcddae681e15d30d3f9de3fa994d10bf3b3
ericblackbird71 <quanyile930@gmail.com>
2025-04-25 10:54:30 +0200

    Create index.js

---
+++ frontend/pages/index.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial, sans-serif" }}>
+      <h1>MUNConnect 模拟联合国平台</h1>
+      <p>欢迎来到 MUNConnect，这里是为班加罗尔的模拟联合国主办方专属打造的平台。</p>
+      <ul>
+        <li>发布活动</li>
+        <li>代表报名</li>
+        <li>自动分配 & 邮件通知</li>
+        <li>二维码签到系统</li>
+        <li>安全在线支付</li>
+      </ul>
+    </div>
+  );
+}
